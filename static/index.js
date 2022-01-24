de071c4ae9f83ca80948609e81f6058f25a93723
20180704 <57553076+20180704@users.noreply.github.com>
2022-01-24 17:22:55 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,7 +341,7 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '逊呐';
+    if (score <= 49) return '好';
     if (score <= 99) return '你是一个一个';
     if (score <= 149) return '哼哼哼';
     if (score <= 199) return '三哼经';
