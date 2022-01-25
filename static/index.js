da7ff89ae49fdefedaa756becaf6d78db9e17421
20180704 <57553076+20180704@users.noreply.github.com>
2022-01-25 16:59:31 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,7 +341,7 @@ function shareText(score) {
         //return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '火自己都灭了';
+    if (score <= 49) return '再慢点火自己都灭了';
     if (score <= 99) return '5楼无了';
     if (score <= 149) return '3栋都被你炸没了';
     if (score <= 199) return '实验都被你扬了';
